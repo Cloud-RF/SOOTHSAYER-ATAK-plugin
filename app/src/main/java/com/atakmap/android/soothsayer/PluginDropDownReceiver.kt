3a5e22aa9de49502cb4d33bb7afae544f6be0535
Alex <alex@cloudrf.com>
2023-06-19 13:14:35 +0100

    Button and help

--- app/src/main/java/com/atakmap/android/soothsayer/PluginDropDownReceiver.kt
+++ app/src/main/java/com/atakmap/android/soothsayer/PluginDropDownReceiver.kt
@@ -127,7 +127,7 @@ class PluginDropDownReceiver(
         }
 
         // add a marker on the map
-        val btnAddMarker = templateView.findViewById<Button>(R.id.btnAddMarker)
+        val btnAddMarker = templateView.findViewById<ImageButton>(R.id.btnAddMarker)
         btnAddMarker.setOnClickListener {
             if (URLUtil.isValidUrl(etServerUrl?.text.toString()) && etApiKey?.text?.trim()
                     ?.isNotEmpty() == true
@@ -440,7 +440,7 @@ class PluginDropDownReceiver(
 
                         override fun onSuccess(response: Any?) {
                             Log.d(TAG, "onSuccess called response: ${Gson().toJson(response)}")
-                            pluginContext.toast(pluginContext.getString(R.string.success_msg))
+                            //pluginContext.toast(pluginContext.getString(R.string.success_msg)) // gets annoying
                             if (response is ResponseModel) {
                                 // download PNG image
                                 repository.downloadFile(response.PNG_WGS84,
